270062571bd76e72b700ef42e9270e25f59b7af7
Dan K <dan.khalid3@gmail.com>
2015-05-18 23:32:12 -0400

    change square size

--- sketchpad.js
+++ sketchpad.js
@@ -1,32 +1,37 @@
 $(document).ready(function(){
 
-    createGrid(2);
 
-     $(document).on('click', '#size', function(){
-	var gridSize = prompt("Enter grid size");
-	createGrid(gridSize);
-    });   
+$(document).on('click', '#size', function(){
+    var gridSize = prompt("Enter grid size");
+    createGrid(gridSize);
+});   
 
-    $(document).on('mouseover', '.square', function(){
-	$(this).css('background-color','red');
-    });
-    
-    $(document).on('mouseleave', '.square', function(){
-	$(this).fadeOut(200, function(){
-    	$(this).css('background-color','white');
-    	$(this).fadeIn(200);
-	});
-    });
-    
-
+$(document).on('mouseover', '.square', function(){
+    $(this).css('background-color','red');
 });
+
+$(document).on('mouseleave', '.square', function(){
+    $(this).fadeOut(200, function(){
+    $(this).css('background-color','white');
+    $(this).fadeIn(200);
+    });
+});
+
+
 
 function createGrid(size){
     $("#container").empty();
+
+    var sqSize = 400/size;
+
     for(var i = 0; i < size; i++){
 	for(var j = 0; j < size; j++){
 	    $("#container").append('<div class="square"></div>');
 	}
     }
 
-};
+    $(".square").width(sqSize);
+    $(".square").height(sqSize);
+}
+
+});
