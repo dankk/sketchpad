801bbd1e66e95ae4e039a1550d1494ea7a7ca69e
Dan K <dan.khalid3@gmail.com>
2015-05-21 01:45:36 -0400

    added random colours

--- sketchpad.js
+++ sketchpad.js
@@ -1,34 +1,47 @@
+var gridSize;
+
 $(document).ready(function(){
 
-    createGrid(16);
+    gridSize = 16;
+    createGrid(gridSize, 'square');
 
 });
     
 $(document).on('click', '#size', function(){
-    var gridSize = prompt("Enter grid size");
-    createGrid(gridSize);
+    gridSize = prompt("Enter grid size");
+    createGrid(gridSize, 'square');
 });   
+
+$(document).on('click', '#random', function(){
+    createGrid(gridSize, 'randSquare');
+});   
+
 
 $(document).on('mouseover', '.square', function(){
     $(this).css('opacity', '100');
     $(this).css('background-color','red');
 });
 
-$(document).on('mouseleave', '.square', function(){
+$(document).on('mouseover', '.randSquare', function(){
+    $(this).css('opacity', '100');
+    $(this).css('background-color','#'+Math.floor(Math.random()*16777215).toString(16));
+}); //colour generator from http://www.paulirish.com/2009/random-hex-color-code-snippets/
+
+$(document).on('mouseleave',(".square, .randSquare"), function(){
 
     $(this).animate({opacity: 0}, 200);
 });
 
 
 
-function createGrid(size){
+function createGrid(size, type){
     $("#container").empty();
 
     var sqSize = 400/size;
 
     for(var i = 0; i < size; i++){
 	for(var j = 0; j < size; j++){
-	    $("#container").append($("<div class='square'></div>").height(sqSize).width(sqSize));
+	    $("#container").append($("<div class=" + type + "></div>").height(sqSize).width(sqSize));
 	}
     }
 
