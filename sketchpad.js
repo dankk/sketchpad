a70b46ac88e17d81f5d668c1a75f238e44f30656
Dan K <dan.khalid3@gmail.com>
2015-05-24 22:07:36 -0400

    trail toggles

--- sketchpad.js
+++ sketchpad.js
@@ -5,16 +5,21 @@ $(document).ready(function(){
     gridSize = 16;
     createGrid(gridSize, 'square');
 
-});
-    
-$(document).on('click', '#size', function(){
-    gridSize = prompt("Enter grid size");
-    createGrid(gridSize, 'square');
-});   
 
-$(document).on('click', '#random', function(){
-    $('#container > div').toggleClass('randSquare');
-});   
+    $("#size").click(function(){
+	gridSize = prompt("Enter grid size");
+	createGrid(gridSize, 'square');
+    });  
+
+    $('#random').click(function(){
+	$('#container > div').toggleClass('randSquare');
+    });   
+});
+
+
+$(document).on('click', '#clear', function(){
+    $('#container > div').css('background-color', 'white');
+}); 
 
 $(document).on('mouseover', '.square', function(){
     $(this).css('opacity', '100');
@@ -26,12 +31,13 @@ $(document).on('mouseover', '.randSquare', function(){
     $(this).css('background-color','#'+Math.floor(Math.random()*16777215).toString(16));
 }); //colour generator from http://www.paulirish.com/2009/random-hex-color-code-snippets/
 
-$(document).on('mouseleave',(".square, .randSquare"), function(){
-
-    $(this).animate({opacity: 0}, 200);
+$(document).on('click', '#trail', function(){
+    $('#container > div').toggleClass('trail');
 });
 
-
+$(document).on('mouseleave', '.trail', function(){
+	$(this).animate({opacity: 0}, 200);
+});
 
 function createGrid(size, type){
     $("#container").empty();
